863d864c421ce7e4805199a1f918f54ee9d84f25
Brian Yu <briehnyu@gmail.com>
2023-02-02 14:33:17 -0500

    More problems

--- leetCode/challengesMedium.js
+++ leetCode/challengesMedium.js
@@ -1,7 +1,16 @@
-/** CHALLENGES [MEDIUM]: Group Anagrams
+/**Group Anagrams
   Given an array of strings strs, group the anagrams together. You can return the answer in any order.
   An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once
  * @param {string[]} strs
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {};
+
+/**Top K Frequent Elements
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {};
