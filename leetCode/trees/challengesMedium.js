4d343cb2f5f176adc93cc0d0e2ddfc9258aa0b32
Brian Yu <briehnyu@gmail.com>
2023-02-09 17:42:12 -0500

    Add folders to reformat repo

---
+++ leetCode/trees/challengesMedium.js
@@ -0,0 +1,47 @@
+/**Group Anagrams
+  Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+  An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {};
+
+/**Top K Frequent Elements
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {};
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {};
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {};
+
+/*
+
+    TWO POINTER SECTION
+
+*/
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {};
