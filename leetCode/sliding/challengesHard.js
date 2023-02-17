aeea64c20582dd744bfe1709251c298d4b4b0dfe
Brian Yu <briehnyu@gmail.com>
2023-02-17 12:53:54 -0500

    More problems

---
+++ leetCode/sliding/challengesHard.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function (s, t) {};
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {};
---
+++ leetCode/sliding/challengesMedium.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {};
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {};
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {};
